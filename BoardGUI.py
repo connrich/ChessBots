339fc0452d3fd10a9a4137c3a400d51773f47c62
Connor <connrich3@gmail.com>
2021-02-16 00:35:50 -0800

    Fixed window size and nicer colours

--- BoardGUI.py
+++ BoardGUI.py
@@ -6,18 +6,19 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__()
         self.board = Board()
         self.setCentralWidget(self.board)
-        self.setFixedSize(QtCore.QSize(self.width(), self.height()))
+        self.setFixedSize(QtCore.QSize(500, 480))
         self.show()
 
 class Board(QtWidgets.QWidget):
     def __init__(self):
         super(Board, self).__init__()
         self.board_layout = QtWidgets.QGridLayout()
+        self.board_layout.setHorizontalSpacing(0)
+        self.board_layout.setVerticalSpacing(0)
 
         blk_bishop = QtGui.QPixmap('blk_bishop.png')
         piece = QtWidgets.QLabel()
         piece.setPixmap(blk_bishop)
-        piece.setStyleSheet('background-color: rgb(255, 255, 255);')
 
         white_square = True
         for i in range(8):
@@ -25,28 +26,32 @@ class Board(QtWidgets.QWidget):
                 blk_bishop = QtGui.QPixmap('blk_bishop.png')
                 piece = QtWidgets.QLabel()
                 piece.setPixmap(blk_bishop)
+                piece.setFixedSize(QtCore.QSize(60, 60))
 
                 square = QtWidgets.QFrame()
+                square.setFixedSize(QtCore.QSize(60, 60))
                 if white_square:
-                    square.setStyleSheet("background-color: rgb(255, 255, 255);")
+                    square.setStyleSheet("background-color: rgb(240, 240, 230);")
                 else:
-                    square.setStyleSheet("background-color: rgb(47, 47, 47);")
+                    square.setStyleSheet("background-color: rgb(118, 150, 86);")
                 white_square = not white_square
-                #piece.setSizePolicy(QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Ignored, QtWidgets.QSizePolicy.Ignored))
+                # piece.setSizePolicy(QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Ignored, QtWidgets.QSizePolicy.Ignored))
                 self.board_layout.addWidget(square, i, j)
+                print(square.size())
                 self.board_layout.addWidget(piece, i, j)
+                print(piece.size())
 
             white_square = not white_square
 
         self.setLayout(self.board_layout)
 
-    def resizeEvent(self, a0: QtGui.QResizeEvent):
-        # QtGui.QResizeEvent(self)
-        # super(MainWindow, self).resizeEvent()
-        if self.width() < self.height():
-            self.setGeometry(QtCore.QRect(0, 0, self.width(), self.width()))
-        elif self.height() > self.width():
-            self.setGeometry(QtCore.QRect(0, 0, self.height(), self.height()))
+    # def resizeEvent(self, a0: QtGui.QResizeEvent):
+    #     # QtGui.QResizeEvent(self)
+    #     # super(MainWindow, self).resizeEvent()
+    #     if self.width() < self.height():
+    #         self.setGeometry(QtCore.QRect(0, 0, self.width(), self.width()))
+    #     elif self.height() > self.width():
+    #         self.setGeometry(QtCore.QRect(0, 0, self.height(), self.height()))
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
