60581a09102d9b7ff254d5251a17be329fc98502
Connor <connrich3@gmail.com>
2021-02-16 15:29:47 -0800

    All pieces on game board and basic evaluation started

--- BoardGUI.py
+++ BoardGUI.py
@@ -1,10 +1,13 @@
 from PyQt5 import QtWidgets, QtGui, QtCore
 import sys
+import chess
+from ChessTest import RandomPlayer
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.board = Board()
+        self.board.boardFromFEN(chess.STARTING_BOARD_FEN)
         self.setCentralWidget(self.board)
         self.setFixedSize(QtCore.QSize(500, 480))
         self.show()
@@ -16,18 +19,11 @@ class Board(QtWidgets.QWidget):
         self.board_layout.setHorizontalSpacing(0)
         self.board_layout.setVerticalSpacing(0)
 
-        blk_bishop = QtGui.QPixmap('blk_bishop.png')
-        piece = QtWidgets.QLabel()
-        piece.setPixmap(blk_bishop)
+        self.loadPiecePixmaps()
 
         white_square = True
         for i in range(8):
             for j in range(8):
-                blk_bishop = QtGui.QPixmap('blk_bishop.png')
-                piece = QtWidgets.QLabel()
-                piece.setPixmap(blk_bishop)
-                piece.setFixedSize(QtCore.QSize(60, 60))
-
                 square = QtWidgets.QFrame()
                 square.setFixedSize(QtCore.QSize(60, 60))
                 if white_square:
@@ -35,26 +31,86 @@ class Board(QtWidgets.QWidget):
                 else:
                     square.setStyleSheet("background-color: rgb(118, 150, 86);")
                 white_square = not white_square
-                # piece.setSizePolicy(QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Ignored, QtWidgets.QSizePolicy.Ignored))
                 self.board_layout.addWidget(square, i, j)
-                print(square.size())
-                self.board_layout.addWidget(piece, i, j)
-                print(piece.size())
-
             white_square = not white_square
-
         self.setLayout(self.board_layout)
 
-    # def resizeEvent(self, a0: QtGui.QResizeEvent):
-    #     # QtGui.QResizeEvent(self)
-    #     # super(MainWindow, self).resizeEvent()
-    #     if self.width() < self.height():
-    #         self.setGeometry(QtCore.QRect(0, 0, self.width(), self.width()))
-    #     elif self.height() > self.width():
-    #         self.setGeometry(QtCore.QRect(0, 0, self.height(), self.height()))
+    def playGame(self):
+        self.Referee = Referee()
+        for i in range(4):
+            board = self.Referee.takeTurn()
+            print(board)
+
+
+    def boardFromFEN(self, fen):
+        # TODO construct visual board from fen
+        ranks = fen.split('/')
+        for rank_index, rank in enumerate(ranks):
+            file_index = 0
+            for char in rank:
+                if char.isalpha():
+                    piece = QtWidgets.QLabel()
+                    piece.setPixmap(self.getPiecePixmap(char))
+                    self.board_layout.addWidget(piece, rank_index, file_index)
+                    file_index += 1
+                elif char.isdigit():
+                    file_index += int(char)
+
+    def getPiecePixmap(self, piece):
+        try:
+            pixmap = self.pixmapDict[piece]  # = self.blk_bishop
+        except:
+            pixmap = QtGui.QPixmap()
+            pixmap.fill(QtGui.QColor(QtCore.Qt.transparent))
+        return pixmap
+
+    def loadPiecePixmaps(self):
+        self.pixmapDict = {}
+        self.pixmapDict['b'] = QtGui.QPixmap('Piece Images/blk_bishop.png')
+        self.pixmapDict['B'] = QtGui.QPixmap('Piece Images/wht_bishop.png')
+        self.pixmapDict['k'] = QtGui.QPixmap('Piece Images/blk_king.png')
+        self.pixmapDict['K']  = QtGui.QPixmap('Piece Images/wht_king.png')
+        self.pixmapDict['n']  = QtGui.QPixmap('Piece Images/blk_knight.png')
+        self.pixmapDict['N']  = QtGui.QPixmap('Piece Images/wht_knight.png')
+        self.pixmapDict['p']  = QtGui.QPixmap('Piece Images/blk_pawn.png')
+        self.pixmapDict['P'] = QtGui.QPixmap('Piece Images/wht_pawn.png')
+        self.pixmapDict['q']  = QtGui.QPixmap('Piece Images/blk_queen.png')
+        self.pixmapDict['Q']  = QtGui.QPixmap('Piece Images/wht_queen.png')
+        self.pixmapDict['r']  = QtGui.QPixmap('Piece Images/blk_rook.png')
+        self.pixmapDict['R'] = QtGui.QPixmap('Piece Images/wht_rook.png')
+
+class Referee(object):
+    # TODO create 2 player instances and check legal rules
+    def __init__(self):
+        self.realtimeboard = chess.Board()
+        self.white = RandomPlayer(self.realtimeboard, chess.WHITE)
+        self.black = RandomPlayer(self.realtimeboard, chess.BLACK)
+
+    def takeTurn(self):
+        print(self.realtimeboard.turn)
+        if self.realtimeboard.turn == chess.WHITE:
+            move = self.white.getMove(self.realtimeboard)
+        elif self.realtimeboard.turn == chess.BLACK:
+            move = self.black.getMove(self.realtimeboard)
+        else:
+            print('error in takeTurn')
+
+        if move in self.realtimeboard.legal_moves:
+            self.realtimeboard.push(move)
+            print(move)
+            print(self.realtimeboard)
+            return self.realtimeboard
+        else:
+            print('Illegal Move:')
+            print(self.realtimeboard)
+            print(move)
+            print(self.realtimeboard.legal_moves)
+            return
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = MainWindow()
+
+    MainWindow.board.playGame()
 
     sys.exit(app.exec_())
--- ChessTest.py
+++ ChessTest.py
@@ -3,12 +3,12 @@ from random import randint
 import time
 
 class RandomPlayer(object):
-    def __init__(self, board):
+    def __init__(self, board, colour):
         self.game_board = board
         self.legal_moves = []
 
     # Pass in the current board state
-    def getRandomMove(self, board):
+    def getMove(self, board):
         self.game_board = board
         self.legal_moves.clear()
         for legal_move in self.game_board.legal_moves:
@@ -19,20 +19,61 @@ class RandomPlayer(object):
 
         return random_move
 
+class BasicEvalPlayer(object):
+    def __init__(self, board, colour):
+        self.colour = colour
+        self.game_board = board
+        self.legal_moves = []
+
+        self.pieceValues = {'p': 1, 'b': 3, 'n': 3, 'r': 5, 'q': 9, 'k': 3.5}
+
+    def evaluateBoardState(self, board):
+        # TODO
+        ranks = board.fen().split('/')
+        ranks[-1] = ranks[-1].split(' ')[0]  # Strips extraneous information
+        wht_eval = 0
+        blk_eval = 0
+        for rank in ranks:
+            for char in rank:
+                if char.isdigit():
+                    pass
+                elif char.isupper():
+                    wht_eval += self.pieceValues[char.lower()]
+                elif char.islower():
+                    blk_eval += self.pieceValues[char]
+                else:
+                    pass
+        print('white eval: ', wht_eval, ', black eval: ', blk_eval)
+
+    def evalLegalMoves(self, board):
+        self.legal_moves.clear()
+        for legal_move in board.legal_moves:
+            self.legal_moves.append(legal_move)
+
+
+
+    def getMove(self, board):
+        self.game_board = board
+        print(self.evaluateBoardState(board))
+        self.legal_moves.clear()
+        for legal_move in self.game_board.legal_moves:
+            self.legal_moves.append(legal_move)
+        move = self.legal_moves[0]
+        return move
 
 if __name__ == "__main__":
 
     realtimeboard = chess.Board()
 
-    white = RandomPlayer(realtimeboard)
-    black = RandomPlayer(realtimeboard)
+    white = RandomPlayer(realtimeboard, chess.WHITE)
+    black = BasicEvalPlayer(realtimeboard, chess.BLACK)
 
     while not realtimeboard.is_game_over():
         colour_to_move = realtimeboard.turn
         if colour_to_move == chess.WHITE:
-            move = white.getRandomMove(realtimeboard)
+            move = white.getMove(realtimeboard)
         elif colour_to_move == chess.BLACK:
-            move = black.getRandomMove(realtimeboard)
+            move = black.getMove(realtimeboard)
 
         legal_moves = realtimeboard.legal_moves
         if move in legal_moves:
